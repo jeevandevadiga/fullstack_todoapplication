57b7d20bbec1eaf80b714efb65cf241cdd137db3
jeevan <devadigajeevan41@gmail.com>
2026-05-17 00:12:05 +0530

    login page created and modification in registration page

---
+++ src/controller/auth/login.auth.js
@@ -0,0 +1,51 @@
+//things that need to be here
+/*
+get the user name and password , validate check if the user exits or not if yes then check the password , then create the token 
+*/
+
+import usermodel from "../../models/user.details.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+async function checklogin(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({ message: "add email and password" });
+    }
+    const validuser = await usermodel.findOne({ email });
+
+    //checking if the user is valid user or not
+    if (!validuser) {
+      return res.status(401).json({ message: "email not found", email });
+    }
+
+    //validating the password
+    let passcheck = await bcrypt.compare(password, validuser.password);
+    if (!passcheck) {
+      return res.status(401).json({ message: "invalid password try again" });
+    }
+
+    //if it passess all the above then it is a valid user then create the cookie
+
+    const token = jwt.sign(
+      { id: validuser._id, email: validuser.email },
+      "7633cc63c8724b4b2bb8617e777fa29fc96202cda8c3010ce680aefb68c1fcb5",
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("usertoken", token);
+
+    res
+      .status(200)
+      .json({ message: `${validuser.username} logged in successfully` });
+  } catch (error) {
+    res
+      .status(401)
+      .json({ message: "failed in the login page", error: error.message });
+  }
+}
+
+
+export default checklogin;
--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userdetails from '../controller/auth/user.auth.js'
+import checklogin from '../controller/auth/login.auth.js'
 const authrouter = express.Router();
 
 authrouter.post("/saveuser",userdetails);
+authrouter.post("/userlogin",checklogin);
 
 
 export default authrouter;
